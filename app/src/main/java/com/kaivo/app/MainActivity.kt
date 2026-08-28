2d07800128e30daa7b75e647f7dfc69018d1346f
BLACK CAT <141953740+voidquanta@users.noreply.github.com>
2026-08-28 15:09:34 +0330

    Update MainActivity.kt

--- app/src/main/java/com/kaivo/app/MainActivity.kt
+++ app/src/main/java/com/kaivo/app/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.kaivo.app
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.kaivo.app.ui.KaivoApp
 
 class MainActivity : AppCompatActivity() {
-
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(
-            AppCompatDelegate.attachBaseContext(newBase)
-        )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
